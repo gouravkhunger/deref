0ba23582ae8ffab4a92b6474d9fe00843629390a
Gourav Khunger <gouravkhunger18@gmail.com>
2022-10-30 19:18:58 +0530

    feat: complete cli

--- cli/bin/index.js
+++ cli/bin/index.js
@@ -1,12 +1,54 @@
 #! /usr/bin/env node
 
-const yargs = require("yargs");
+const axios = require("axios");
+const { program } = require("commander");
+const cliInfo = require("../package.json");
 
-const options = yargs  
-  .usage("Usage: deref <cmd> [args]")  
-  .command("link", {
-    alias:"link",
-    describe: "Link to trace for redirects.",
-    type: "string",
-    demandOption: true
-  }).help(true).argv;
+program
+  .name("deref")
+  .description("CLI interface for https://deref.gouravkhunger.me")
+  .version(cliInfo["version"], '-v, --version');
+
+program
+.requiredOption('-u, --url <string>', 'url to trace redirection path')
+.option('-j, --json', 'output raw json response');
+
+program.parse();
+
+const options = program.opts();
+const url = options.url;
+const json = options.json ? true : false;
+
+axios.post("https://deref.gouravkhunger.me/api", new URLSearchParams({
+  'link': url
+})).then((response) => {
+  const data = response.data;
+  if (json) {
+    console.log(data);
+    return;
+  }
+
+  const pluralize = data.redirect_count == 1 ? "time" : "times";
+
+  console.log(`Start url: ${data.start_url}`);
+  console.log(`Final url: ${data.final_url}\n`);
+
+  console.log(`Found ${data.redirect_count} redirects:`);
+
+  for (let i = 0; i < data.route_log.length; i++) {
+    console.log(`${i} — ${data.route_log[i]}`);
+  }
+}).catch((error) => {
+  const response = error.response;
+
+  if (json) {
+    console.log({
+      status: response.status,
+      ...response.data
+    });
+    process.exit(1);
+  }
+
+  console.log(`An unexpected ${response.status} error occurred. ${response.data.message}`);
+  process.exit(1);
+});
